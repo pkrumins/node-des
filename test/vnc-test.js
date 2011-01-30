e7bada6a1db6708d48c6fd873b4142327dfd72bc
James Halliday <substack@gmail.com>
2011-01-30 08:12:52 +0000

    expresso test passes!

--- test/vnc-test.js
+++ test/vnc-test.js
@@ -7,11 +7,5 @@ var response = fs.readFileSync(__dirname + '/response')
 
 exports.challenge_response = function () {
     var encrypted = des.encrypt('browsers', challenge);
-    
-    console.log(encrypted);
-    console.log(response);
+    assert.eql(encrypted, response);
 };
-
-if (__filename === process.argv[1]) {
-    exports.challenge_response();
-}
